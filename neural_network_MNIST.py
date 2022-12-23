e53d7413c9d79032d734459feb07c6da02388ecb
Schlafi <alanvergel.ai@gmail.com>
2022-12-23 17:36:35 -0600

    Add prediction step.

--- neural_network_MNIST.py
+++ neural_network_MNIST.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Dense
@@ -24,6 +26,12 @@ print("Shape of one single y:",y_train[0:1].shape)
 print("First 10 digits:",y_train[0:10])
 print("y_train shape: ", y_train.shape)
 
+#Show examples
+#Images 
+example = 35
+imgplot = plt.imshow(x_train[example,:])
+plt.show()
+
 #Normalize data between 0 and 1
 x_train = x_train/255.0
 
@@ -45,9 +53,12 @@ model.build(input_shape)
 print("Model summary: ")
 model.summary()
 
-#Train the model.
-print("Training ...")
-model.fit(x_train,y_train,epochs = 4, batch_size = 32)
+#Change to activate/deactivate training
+TRAIN = False
+if TRAIN:
+    #Train the model.
+    print("Training ...")
+    model.fit(x_train,y_train,epochs = 4, batch_size = 32)
 
 #Normalize data between 0 and 1
 x_test = x_test/255.0
@@ -56,4 +67,13 @@ print("Evaluating on test data...")
 #Eval the model on test set.
 model.evaluate(x_test,y_test,batch_size = 32)
 
+
+#Prediction
+#Add batch dimention to example (1,28,28)
+pred = model(x_train[example][None,:,:])
+#Can make batch predictions. Several predictions at one 
+#100 predictions :D
+#pred = model(x_train[10:110])
+print("Neural net output:", pred)
+print("Digit predicted: ",int(tf.math.argmax(pred,axis = 1)))
 
